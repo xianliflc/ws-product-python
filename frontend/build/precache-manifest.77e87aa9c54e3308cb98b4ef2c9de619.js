931d1896af2d9c7e4e060b50cf228ce77a5ec7e1
Xian Li <xian@smooth.tech>
2020-01-12 10:39:54 -0500

    update build files

---
+++ frontend/build/precache-manifest.77e87aa9c54e3308cb98b4ef2c9de619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67dfc098b5dd56710400e15d3eb1f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddc7227395c7951860a8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a00fb39b580479d761a4",
+    "url": "/static/js/2.fe0892be.chunk.js"
+  },
+  {
+    "revision": "b3dfc9170f9918ea3e94e50ca041ebd8",
+    "url": "/static/js/2.fe0892be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddc7227395c7951860a8",
+    "url": "/static/js/main.c281fc03.chunk.js"
+  },
+  {
+    "revision": "b5ff61001688404db26f",
+    "url": "/static/js/runtime-main.8c5f780f.js"
+  }
+]);
